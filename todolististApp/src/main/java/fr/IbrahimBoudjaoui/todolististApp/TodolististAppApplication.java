6f9321164584e9c4c8607de36ed474b226fb18b1
ibrahimbdj <ibrahim.adresse.pro@gmail.com>
2025-05-14 22:10:21 +0200

    first commi

---
+++ todolististApp/src/main/java/fr/IbrahimBoudjaoui/todolististApp/TodolististAppApplication.java
@@ -0,0 +1,13 @@
+package fr.IbrahimBoudjaoui.todolististApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolististAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolististAppApplication.class, args);
+	}
+
+}
---
+++ todolististApp/src/test/java/fr/IbrahimBoudjaoui/todolististApp/TodolististAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.IbrahimBoudjaoui.todolististApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolististAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
