63e7f36f72f623dddb47bda83a1edf4bf99178c1
ibrahimbdj <ibrahim.adresse.pro@gmail.com>
2025-05-17 01:19:17 +0200

    ajout 2eme microservice: génération mdp sécu

---
+++ passwd_generation/src/main/java/com/ibrahimbdj/passwd_generation/PasswdGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahimbdj.passwd_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswdGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswdGenerationApplication.class, args);
+	}
+
+}
---
+++ passwd_generation/src/test/java/com/ibrahimbdj/passwd_generation/PasswdGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimbdj.passwd_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswdGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
